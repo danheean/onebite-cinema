4be18fa09ffc7d3f52018c733018087850c6465e
jurepi <danhee.an@gmail.com>
2025-04-15 20:26:11 +0900

    파비콘과 썸네일 이미지 추가, 페이지 메타데이터 개선 및 Head 컴포넌트 적용. 인덱스, 영화 상세, 검색 페이지에 Open Graph 메타 태그 추가.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import style from "./index.module.css";
 import { InferGetStaticPropsType } from "next";
 import fetchMovies from "@/lib/fetch-movies";
 import fetchRandomMovies from "@/lib/fetch-random-movies";
-
+import Head from "next/head";
 export const getStaticProps = async () => {
   console.log("인덱스 페이지");
   const [allMovies, recommendedMovies] = await Promise.all([
@@ -15,7 +15,6 @@ export const getStaticProps = async () => {
 
   return {
     props: { allMovies, recommendedMovies },
-    revalidate: 3,
   };
 };
 
@@ -31,22 +30,33 @@ export default function Home({
   }, []);
 */
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.movie_recommend_list}>
-          {recommendedMovies.map((movie) => (
+    <>
+      <Head>
+        <title>한입 씨네마</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입 씨네마" />
+        <meta
+          property="og:description"
+          content="한입 씨네마에 등록된 영화를 만나보세요."
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 가장 추천하는 영화</h3>
+          <div className={style.movie_recommend_list}>
+            {recommendedMovies.map((movie) => (
+              <MovieItem key={movie.id} {...movie} />
+            ))}
+          </div>
+        </section>
+        <section>등록된 모든 영화</section>
+        <div className={style.movie_list}>
+          {allMovies.map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
-      </section>
-      <section>등록된 모든 영화</section>
-      <div className={style.movie_list}>
-        {allMovies.map((movie) => (
-          <MovieItem key={movie.id} {...movie} />
-        ))}
       </div>
-    </div>
+    </>
   );
 }
 
